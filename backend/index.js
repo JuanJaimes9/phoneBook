51b803fe6116cdf1a479341bc788f8f90d9c2bfb
Juan Jaimes <juandiegojaimesp99@gmail.com>
2024-08-16 21:27:04 -0500

    dist files

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(express.static("dist"));
 
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
